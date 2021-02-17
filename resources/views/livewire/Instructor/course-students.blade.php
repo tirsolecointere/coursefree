81e6cacbb95ad7c71810edf31e6330581eb95dc0
Tirso Lecointere <tirsolecointere@gmail.com>
2021-02-17 01:21:59 -0400

    quick fix for students empty state

--- resources/views/livewire/Instructor/course-students.blade.php
+++ resources/views/livewire/Instructor/course-students.blade.php
@@ -23,28 +23,28 @@
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
                 @forelse ($students as $student)
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0 h-10 w-10">
-                                <img class="h-8 w-8 rounded-full" src="{{ $student->profile_photo_url }}">
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <div class="flex-shrink-0 h-10 w-10">
+                                    <img class="h-8 w-8 rounded-full" src="{{ $student->profile_photo_url }}">
+                                </div>
+                                <div class="ml-4">
+                                    <div class="text-sm text-gray-500">{{ $student->name}}</div>
+                                </div>
                             </div>
-                            <div class="ml-4">
-                                <div class="text-sm text-gray-500">{{ $student->name}}</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-900">{{ $student->email }}</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="" class="text-indigo-600 hover:text-indigo-900">Ver</a>
-                    </td>
-                </tr>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900">{{ $student->email }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <a href="" class="text-indigo-600 hover:text-indigo-900">Ver</a>
+                        </td>
+                    </tr>
                 @empty
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium" colspan="5">No se encontraron registros.</td>
-                </tr>
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium" colspan="3">No hay ningun estudiante matriculado a este curso.</td>
+                    </tr>
                 @endforelse
             </tbody>
         </table>
